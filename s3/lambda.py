f2199bbd36fe1d2479088e1c3d9297e52bd2983f
nagashima-omonao <omonao-business@gmail.com>
2021-01-03 19:46:11 +0900

    modified

--- s3/lambda.py
+++ s3/lambda.py
@@ -1,7 +1,9 @@
 import json
 import requests
 import xml.etree.ElementTree as et
-
+import boto3
+import pandas as pd
+import io
 
 def get_location_gc(address):
   url_gc = 'https://www.geocoding.jp/api?q={addr}'.format(addr=address)
@@ -24,4 +26,26 @@ def lambda_handler(event, context):
     res_rm = requests.get(url_rm, headers=headers)
     
     result = json.loads(res_rm.text)
+    
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket='calc-chika', Key='citycode.csv')
+    value = response['Body'].read().decode('utf-8')
+    df = pd.read_csv(io.StringIO(value), encoding='utf-8')
+    df.columns = ['city_code', 'prefecture', 'city', 'prefecture_kana', 'city_kana']
+    
+    response = s3_client.get_object(Bucket='calc-chika', Key='citycode_2.csv')
+    value = response['Body'].read().decode('utf-8')
+    df_2 = pd.read_csv(io.StringIO(value), encoding='utf-8', header=None, names=['city_code', 'city', 'city_kana'])
+    
+    for i, r in enumerate(result['values']):
+        city_code = str(r['city_code'])
+        tgt = df[[str(cc).startswith(city_code) for cc in df['city_code']]]
+        if len(tgt) > 0:
+            result['values'][i]['city_name'] = tgt.iloc[0]['prefecture'] + tgt.iloc[0]['city']
+        else:
+            tgt = df_2[[str(cc).startswith(city_code) for cc in df_2['city_code']]]
+            if len(tgt) > 0:
+                result['values'][i]['city_name'] = tgt.iloc[0]['city']
+            
+    
     return result['values']
